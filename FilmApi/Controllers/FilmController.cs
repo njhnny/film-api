2a053eca37a882d71cd2696d65a988bbf8cdceb8
Nick Hennessy <njhnny@gmail.com>
2022-03-06 18:53:00 -0800

    update controller to include get without id, update and delete

--- FilmApi/Controllers/FilmController.cs
+++ FilmApi/Controllers/FilmController.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FilmApi.Models;
@@ -17,19 +20,53 @@ namespace FilmApi.Controllers
       _db = db;
     }
 
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Film>>> Get()
+    {
+      return await _db.Films.ToListAsync();
+    }
+
     [HttpGet("{id}")]
     public async Task<ActionResult<Film>> GetFilm(int id)
     {
-        var film = await _db.Films.FindAsync(id);
+      var film = await _db.Films.FindAsync(id);
 
-        if (film == null)
-        {
-            return NotFound();
-        }
+      if (film == null)
+      {
+        return NotFound();
+      }
 
-        return film;
+      return film;
     }
 
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Film film)
+    {
+      if (id != film.FilmId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(film).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!FilmExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
 
     [HttpPost]
     public async Task<ActionResult<Film>> Post(Film film)
@@ -38,6 +75,26 @@ namespace FilmApi.Controllers
       await _db.SaveChangesAsync();
 
       return CreatedAtAction("Post", new { id = film.FilmId }, film);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteFilm(int id)
+    {
+      var film = await _db.Films.FindAsync(id);
+      if (film == null)
+      {
+        return NotFound();
+      }
+
+      _db.Films.Remove(film);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool FilmExists(int id)
+    {
+      return _db.Films.Any(e => e.FilmId == id);
     }
   }
 }
